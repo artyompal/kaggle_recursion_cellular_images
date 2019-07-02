7a7589d9c7d58f520a5ed839d6b50932b4b6482f
artyom <cppg@cppg.co.uk>
2019-07-02 13:05:23 +0100

    implemented data loader

--- data_loader.py
+++ data_loader.py
@@ -14,82 +14,109 @@ import pandas as pd
 import torch
 import torch.utils.data
 import torchvision.transforms as transforms
+import albumentations as albu
 
 from PIL import Image
+from tqdm import tqdm
+from debug import dprint
 
 
 SAVE_DEBUG_IMAGES = False
 
 
-class ImageDataset(torch.utils.data.Dataset):
-    def __init__(self, dataframe: pd.DataFrame, mode: str, config: Any,
-                 num_ttas: int = 1, augmentor: Any = None,
-                 aug_type: str = 'albu') -> None:
+class ImageDataset(torch.utils.data.Dataset): # type: ignore
+    def __init__(self, dataframe: pd.DataFrame, controls_df: pd.DataFrame,
+                 mode: str, config: Any, num_ttas: int = 1, augmentor: Any = None) -> None:
         print(f'creating data_loader for {config.version} in mode={mode}')
         assert mode in ['train', 'val', 'test']
 
         self.df = dataframe
         self.mode = mode
-        self.augmentor = augmentor
-        self.aug_type = aug_type
 
         self.version = config.version
         self.path = config.data.train_dir if mode != 'test' else config.data.test_dir
         self.num_classes = config.model.num_classes
-        self.input_size = config.model.input_size
-        self.rect_crop = config.data.rect_crop
+        self.image_size = config.model.image_size
         self.num_ttas = num_ttas
+        self.num_channels = config.model.num_channels
 
-        if 'ception' in config.model.arch:
-            self.transforms = transforms.Compose([
-                transforms.ToTensor(),
-                transforms.Normalize(mean=[0.5, 0.5, 0.5],
-                                      std=[0.5, 0.5, 0.5])
-            ])
-        else:
-            self.transforms = transforms.Compose([
-                transforms.ToTensor(),
-                transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                      std=[0.229, 0.224, 0.225]),
-            ])
 
-    def _transform_image(self, image: Image, index: int) -> torch.Tensor:
-        image = np.array(image)
+        # load negative control information
+        self.neg_control: Dict[str, str] = {}
+        controls_df = controls_df.loc[controls_df.well_type == 'negative_control']
 
-        if self.rect_crop.enable:
-            dims = image.shape[:2]
-            biggest_size, smallest_size = max(dims), min(dims)
+        for row in controls_df.itertuples():
+            exp_plate = f'{row.experiment}_{row.plate}'
+            self.neg_control[exp_plate] = row.well
 
-            assert self.rect_crop.min_ratio <= self.rect_crop.max_ratio
-            assert self.rect_crop.max_ratio <= 1.0
+        # check if there's negative control for every sample
+        for row in self.df.itertuples():
+            exp_plate = f'{row.experiment}_{row.plate}'
+            assert self.neg_control[exp_plate] != ''
 
-            ratio = random.uniform(self.rect_crop.min_ratio, self.rect_crop.max_ratio)
-            crop_big_size = min(int(self.input_size / ratio), biggest_size)
-            crop_small_size = min(self.input_size, smallest_size)
 
-            crop_size = (crop_big_size, crop_small_size) if biggest_size == dims[0] \
-                        else (crop_small_size, crop_big_size)
+        # if we use augmentations, create group transform
+        self.augmentor = None
 
-            y = int(random.uniform(0, dims[0] - crop_size[0]))
-            x = int(random.uniform(0, dims[1] - crop_size[1]))
-            image = image[y : y + crop_size[0], x : x + crop_size[1]]
+        if augmentor is not None:
+            targets = {'image1': 'image'}
+            self.augmentor = albu.Compose(augmentor, p=1,
+                                          additional_targets=targets)
 
-            if self.rect_crop.scale_both_dims:
-                new_size = (self.input_size, self.input_size)
-            else:
-                new_size = (min(crop_size[0], self.input_size),
-                            min(crop_size[1], self.input_size))
+        self.transforms = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.5] * 6, std=[0.5] * 6)
+        ])
 
-            # print(f'dims were {dims[0]}x{dims[1]}, crop {crop_size[0]}' +
-            #       f'x{crop_size[1]}, scaled into {new_size[0]}x{new_size[1]}')
+        # if 'ception' in config.model.arch:
+        #     self.transforms = transforms.Compose([
+        #         transforms.ToTensor(),
+        #         transforms.Normalize(mean=[0.5, 0.5, 0.5],
+        #                               std=[0.5, 0.5, 0.5])
+        #     ])
+        # else:
+        #     self.transforms = transforms.Compose([
+        #         transforms.ToTensor(),
+        #         transforms.Normalize(mean=[0.485, 0.456, 0.406],
+        #                               std=[0.229, 0.224, 0.225]),
+        #     ])
 
-            image = np.array(Image.fromarray(image).resize(new_size, Image.BICUBIC))
+    def _load_image(self, path: str) -> np.array:
+        ''' Loads image into np.array with optional resize. '''
+        image = Image.open(path)
 
+        if self.image_size != 0:
+            image = image.resize((self.image_size, self.image_size), Image.LANCZOS)
+
+        return np.array(image)
+
+    def _load_images(self, index: int) -> np.array:
+        ''' Loads two N-channel images and stacks them. '''
+        df_index, site = index // 2, index % 2
+        exp, plate, well = self.df.iloc[df_index, 1], self.df.iloc[df_index, 2], \
+                           self.df.iloc[df_index, 3],
+        layers = []
+
+        for channel in range(self.num_channels):
+            filename = f'{exp}/Plate{plate}/{well}_s{site+1}_w{channel+1}.png'
+            layers.append(self._load_image(os.path.join(self.path, filename)))
+
+        neg_ctl_well = self.neg_control[f'{exp}_{plate}']
+
+        for channel in range(self.num_channels):
+            filename = f'{exp}/Plate{plate}/{neg_ctl_well}_s{site+1}_w{channel+1}.png'
+            layers.append(self._load_image(os.path.join(self.path, filename)))
+
+        image = np.dstack(layers)
+        return image
+
+    def _transform_images(self, image: np.array, index: int) -> torch.Tensor:
+        ''' Applies augmentations, if any. '''
         if self.augmentor is not None:
-            if self.aug_type == 'albu':
-                image = self.augmentor(image=image)['image']
-            elif self.aug_type == 'imgaug':
-                image = self.augmentor.augment_image(image)
+            image0, image1 = image[:, :, :self.num_channels], image[:, :, self.num_channels:]
+            results = self.augmentor(image=image0, image1=image1)
+            image0, image1 = results['image'], results['image1']
+            image = np.dstack([image0, image1])
 
         if SAVE_DEBUG_IMAGES:
             os.makedirs(f'../debug_images_{self.version}/', exist_ok=True)
@@ -99,14 +126,12 @@ class ImageDataset(torch.utils.data.Dataset):
 
     def __getitem__(self, index: int) -> Any:
         ''' Returns: tuple (sample, target) '''
-        filename = self.df.iloc[index, 0]
-        image = Image.open(os.path.join(self.path, filename + '.png'))
-        assert image.mode == 'RGB'
+        image = self._load_images(index)
 
         if self.num_ttas == 1:
-            image = self._transform_image(image, index)
+            image = self._transform_images(image, index)
         else:
-            crops = [self._transform_image(image, index) for _ in range(self.num_ttas)]
+            crops = [self._transform_images(image, index) for _ in range(self.num_ttas)]
 
             for i in range(len(crops)):
                 if i % 2 != 0:
@@ -115,12 +140,11 @@ class ImageDataset(torch.utils.data.Dataset):
             image = torch.stack(crops)
 
         if self.mode != 'test':
-            targets = np.zeros(self.num_classes, dtype=np.float32)
-            labels = list(map(int, self.df.iloc[index, 1].split()))
-            targets[labels] = 1
+            targets = int(self.df.sirna.values[index // 2])
             return image, targets
         else:
             return image
 
     def __len__(self) -> int:
-        return self.df.shape[0]
+        ''' We have two sets of images per well. '''
+        return self.df.shape[0] * 2
